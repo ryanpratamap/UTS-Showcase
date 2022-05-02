54cdf6f935275fcfb605625a01bab68de4f2127b
Ryan Pratama Pangestu Lo <ryan.p.pangestu@gmail.com>
2022-05-03 00:22:41 +1000

    Initial Commit

---
+++ UTS-Showcase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UTS-Showcase
+//
+//  Created by Ryan Pratama Pangestu Lo on 3/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
