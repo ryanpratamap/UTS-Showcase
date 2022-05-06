bcb794a74b14b4aa8ead614368f38f1704a2c2db
Elizabeth Kim <wkim2134@gmail.com>
2022-05-06 11:32:03 +1000

    created struct Events

--- UTS-Showcase/Controller/EventViewController.swift
+++ UTS-Showcase/Controller/EventViewController.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+//All events
 
 class EventViewController: UIViewController {
 
---
+++ UTS-Showcase/Model/Events.swift
@@ -0,0 +1,16 @@
+//
+//  Events.swift
+//  UTS-Showcase
+//
+//  Created by Elizabeth Kim on 6/5/22.
+//
+
+import Foundation
+import UIKit
+
+struct Events: Codable {
+    var title: String
+    var date: String
+    var time: String
+    var description: String
+}
