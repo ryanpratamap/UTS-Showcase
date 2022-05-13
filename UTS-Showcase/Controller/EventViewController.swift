5928ce6947ad917131bfc39c3682a95f087280fd
Adrian Crespo Sanjines <adriancresposanjines@gmail.com>
2022-05-13 13:24:04 +1000

    Added comments to Event View Controller file

--- UTS-Showcase/Controller/EventViewController.swift
+++ UTS-Showcase/Controller/EventViewController.swift
@@ -3,52 +3,52 @@
 //  UTS-Showcase
 //
 //  Created by Ryan Pratama Pangestu Lo on 3/5/22.
+//  Modified by Adrian Crespo Sanjines
 //
 
 import Foundation
 import UIKit
 
+//User defaults key for event list
 let EVENTS_LIST_KEY = "events"
 
 class EventViewController: UIViewController {
-    
+    //Outlet connections for tableView and label
     @IBOutlet weak var eventTableView: UITableView!
     @IBOutlet weak var welcomeLabel: UILabel!
-    //var name:String?
+    //Event list of type Event
     var events: [Event] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //welcomeLabel.text = "Welcome, \(name!)!"
+        //Write 3 initial events to user defaults
         writeEvents()
+        //Set the table view cell height to 225
         eventTableView.rowHeight = 225
+        //Get the user defaults event list to update the event list
         events = readEvents()
     }
-
+    
+    /*
+     Function to write events to user defaults
+     Writes the initial three events to user defaults list
+     */
     func writeEvents() {
-        // Write high scores to User Defautls
         let defaults = UserDefaults.standard
-        //var updatedEvents = readEvents()
-        
-        //if (name != nil) {
-        //updatedHighScoreFromGame.append(GameScore(name: name!, score: score!))
+        //Updated event list to be sent to user defaults
         let updatedEvents = (
             [Event(title: "Engineering Prototype Exhibition", date: "June 28, 2022", time: "15:00pm - 18:00pm", location: "Building 11", description: "Blah blah blah blah", imageName: "Engineering",  noOfTickets: 1), Event(title: "Architecture Prototype Exhibition", date: "June 29, 2022", time: "15:00pm - 18:00pm", location: "Building 11", description: "Blah blah blah blah", imageName: "Architecture", noOfTickets: 1), Event(title: "IT Prototype Exhibition", date: "June 27, 2022", time: "15:00pm - 18:00pm", location: "Building 11", description: "Blah blah blah blah", imageName: "IT", noOfTickets: 1)]
         )
-        //}
-        
+        //Set the updated list as the event list in user defaults
         defaults.set(try? PropertyListEncoder().encode(updatedEvents), forKey: EVENTS_LIST_KEY)
     }
     /*
-     
+     Function to read event list from user defaults
+     Returns a list of type Event
      */
     func readEvents() -> [Event] {
-        // Read from User Defaults
-        // This should happen at the HighScrollViewController
-        
         let defaults = UserDefaults.standard;
-        
+        //Read from user defaults and return array
         if let savedArrayData = defaults.value(forKey:EVENTS_LIST_KEY) as? Data {
             if let array = try? PropertyListDecoder().decode(Array<Event>.self, from: savedArrayData) {
                 return array
@@ -62,7 +62,7 @@ class EventViewController: UIViewController {
 }
 
 extension EventViewController: UITableViewDataSource {
-    
+    //The table view will display n events, where n is the current length of the events list
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return events.count;
     }
@@ -72,9 +72,10 @@ extension EventViewController: UITableViewDataSource {
         // Dequed a reusable cell from the table view
         let cell = eventTableView.dequeueReusableCell(withIdentifier: "eventObj", for: indexPath) as! EventsTVC;
         
-        // Updated the UI for this Cell
+        // Update the UI for this Cell
         let eventInstance = events[indexPath.row]
-        
+        //By setting the tableCell to be of type EventsTVC we can access each attribute from the custom cell
+        //Events TVC is a CocoaTouch file
         cell.eventNameLabel.text = eventInstance.title;
         cell.eventDateLabel.text = eventInstance.date;
         cell.eventTimeLabel.text = eventInstance.time;
@@ -89,11 +90,11 @@ extension EventViewController: UITableViewDataSource {
 extension EventViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //grab pressed event
+        //Select pressed event
         let eventInstance = events[indexPath.row]
-        
+        //Instantiate the Detail View Controller
         let vc = storyboard?.instantiateViewController(identifier: "DetailViewController") as! DetailViewController
-        
+        //Send all respective data from the selected event
         vc.detailTitle = eventInstance.title
         vc.detailTime = eventInstance.time
         vc.detailLocation = eventInstance.location
@@ -101,7 +102,7 @@ extension EventViewController: UITableViewDelegate {
         vc.detailNumberOfTickets = eventInstance.noOfTickets
         vc.detailDate = eventInstance.date
         vc.detailDescription = eventInstance.description
-        
+        //Transition to Detail View Controller
         self.navigationController?.pushViewController(vc, animated: true)
         vc.navigationItem.setHidesBackButton(false, animated: true)
         
